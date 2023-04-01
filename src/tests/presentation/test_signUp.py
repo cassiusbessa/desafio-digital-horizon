4282a921f8defc51a64486d2ba84c0aef2996627
cassiusbessa <cassiusbessa@gmail.com>
2023-04-01 16:55:13 -0300

    test: :white_check_mark: ensure Signup calls useCase with corrects params

--- src/tests/presentation/test_signUp.py
+++ src/tests/presentation/test_signUp.py
@@ -9,31 +9,45 @@ from presentation.controllers.signUp import SignUp
 class TestSignUp(unittest.TestCase):
     def setUp(self):
         self.useCases = MagicMock(spec=UserUseCases)
-        self.controller = SignUp(self.useCases)
+        self.sut = SignUp(self.useCases)
 
-    def test_handle_missing_name(self):
+    async def test_handle_missing_name(self):
         request = HttpRequest(
             body={"email": "any_email@email.com", "password": "any_password"}
         )
 
-        response = self.controller.handle(request)
+        response = await self.sut.handle(request)
         self.assertEqual(response.statusCode, 400)
         self.assertEqual(response.body, {"error": "Missing param: name"})
 
-    def test_handle_missing_email(self):
+    async def test_handle_missing_email(self):
         request = HttpRequest(
             body={"name": "any_name", "password": "any_password"}
         )
 
-        response = self.controller.handle(request)
+        response = await self.sut.handle(request)
         self.assertEqual(response.statusCode, 400)
         self.assertEqual(response.body, {"error": "Missing param: email"})
 
-    def test_handle_missing_password(self):
+    async def test_handle_missing_password(self):
         request = HttpRequest(
             body={"name": "any_name", "email": "any_email@email.com"}
         )
 
-        response = self.controller.handle(request)
+        response = await self.sut.handle(request)
         self.assertEqual(response.statusCode, 400)
         self.assertEqual(response.body, {"error": "Missing param: password"})
+
+    async def test_handle_calls_use_case_with_correct_params(self):
+        request = HttpRequest(
+            body={
+                "name": "any_name",
+                "email": "any_email@email.com",
+                "password": "any_password",
+            }
+        )
+
+        await self.sut.handle(request)
+        self.useCases.addAccount.assert_called_once_with(
+            "any_name", "any_email@email.com", "any_password"
+        )
