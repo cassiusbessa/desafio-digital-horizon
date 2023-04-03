6a0e775310db6ed2d2716a0f2b81a45fe4ab43d3
cassiusbessa <cassiusbessa@gmail.com>
2023-04-03 02:52:14 -0300

    test: :white_check_mark: ensure Login returns a 400 error code and missing parameter message if any parameter is missing

---
+++ src/tests/presentation/test_login.py
@@ -0,0 +1,32 @@
+import unittest
+from unittest.mock import MagicMock
+from domain.usecases.authentication import Authentication
+
+from presentation.controllers.protocols.controller import HttpRequest
+from presentation.controllers.login import Login
+
+
+class TestLogin(unittest.IsolatedAsyncioTestCase):
+    def setUp(self):
+        self.authentication = MagicMock(spec=Authentication)
+        self.sut = Login(self.authentication)
+
+    async def test_handle_missing_email(self):
+        request = HttpRequest(
+            body={
+                "password": "any_password",
+            }
+        )
+        response = await self.sut.handle(request)
+        self.assertEqual(response.statusCode, 400)
+        self.assertEqual(response.body, {"error": "Missing param: email"})
+
+    async def test_handle_missing_password(self):
+        request = HttpRequest(
+            body={
+                "email": "any_email",
+            }
+        )
+        response = await self.sut.handle(request)
+        self.assertEqual(response.statusCode, 400)
+        self.assertEqual(response.body, {"error": "Missing param: password"})
