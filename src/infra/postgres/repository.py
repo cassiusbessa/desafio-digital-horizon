1341c02e51991b0c859adc7e03d4324a2ab188f1
cassiusbessa <cassiusbessa@gmail.com>
2023-04-02 21:29:31 -0300

    feat: :sparkles: ensure UserRepository add method save a user in db and return a user

---
+++ src/infra/postgres/repository.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+from data.protocols.userDbProtocols import AddUserRepository
+
+from domain.entities.user import User
+from domain.usecases.addUser import AddUserModel
+
+
+class UserRepository(AddUserRepository):
+    def __init__(self, conn):
+        self.conn = conn
+
+    def add(self, user: AddUserModel) -> User:
+        with self.conn.cursor() as cur:
+            cur.execute(
+                "INSERT INTO users (fullname, email, password, created_at) "
+                "VALUES (%s, %s, %s, %s) RETURNING *;",
+                (
+                    user["fullname"],
+                    user["email"],
+                    user["password"],
+                    datetime.now(),
+                ),
+            )
+            result = cur.fetchone()
+            self.conn.commit()
+            user = User(*result)
+            return user
