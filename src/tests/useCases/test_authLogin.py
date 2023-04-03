def32122fcb67546459855cc53377d3ddecd1a05
cassiusbessa <cassiusbessa@gmail.com>
2023-04-03 04:55:58 -0300

    test: :white_check_mark: ensure dbAuthLogin calls tokenGenerator with corrects params

--- src/tests/useCases/test_authLogin.py
+++ src/tests/useCases/test_authLogin.py
@@ -26,3 +26,13 @@ class TestAuthLogin(unittest.IsolatedAsyncioTestCase):
         self.loadUserByEmailRepository.load.assert_called_with(
             credentials["email"]
         )
+
+    async def test_should_call_tokenGenerator_with_correct_data(self):
+        credentials = {
+            "email": "any_email@email.com",
+            "password": "any_password",
+        }
+        await self.sut.auth(credentials)
+        self.tokenGenerator.generateToken.assert_called_with(
+            await self.loadUserByEmailRepository.load(credentials["email"])
+        )
