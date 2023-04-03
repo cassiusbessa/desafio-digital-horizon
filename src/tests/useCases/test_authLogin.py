7ad01422a303e7750e754a1597803eec8a9808ee
cassiusbessa <cassiusbessa@gmail.com>
2023-04-03 04:46:06 -0300

    test: :white_check_mark: ensure dbAuthLogin calls loadUserByEmailRepository with corrects params

---
+++ src/tests/useCases/test_authLogin.py
@@ -0,0 +1,28 @@
+import unittest
+from unittest.mock import MagicMock
+from data.dbAuthLogin import DbAddLogin
+from data.protocols.cryptographyProtocols import TokenGenerator
+from data.protocols.userDbProtocols import LoadUserByEmailRepository
+
+
+class TestAuthLogin(unittest.IsolatedAsyncioTestCase):
+    def setUp(self):
+        self.loadUserByEmailRepository = MagicMock(
+            spec=LoadUserByEmailRepository
+        )
+        self.tokenGenerator = MagicMock(spec=TokenGenerator)
+        self.sut = DbAddLogin(
+            self.loadUserByEmailRepository, self.tokenGenerator
+        )
+
+    async def test_should_call_loadUserByEmailRepository_with_correct_email(
+        self,
+    ):
+        credentials = {
+            "email": "any_email@email.com",
+            "password": "any_password",
+        }
+        await self.sut.auth(credentials)
+        self.loadUserByEmailRepository.load.assert_called_with(
+            credentials["email"]
+        )
