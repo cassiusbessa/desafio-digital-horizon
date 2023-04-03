9dddcdef59acb4dbd58c1d9c2507cca2f91cc526
cassiusbessa <cassiusbessa@gmail.com>
2023-04-03 19:32:36 -0300

    test: :white_check_mark: ensure dbAddUser usecase calls loadUSerByEmailRepository with corrects params

--- src/tests/useCases/test_addUser.py
+++ src/tests/useCases/test_addUser.py
@@ -1,13 +1,21 @@
 import unittest
 from unittest.mock import MagicMock
 from data.dbAddUser import DbAddUser
-from data.protocols.userDbProtocols import AddUserRepository
+from data.protocols.userDbProtocols import (
+    AddUserRepository,
+    LoadUserByEmailRepository,
+)
 
 
 class TestDbAddUser(unittest.IsolatedAsyncioTestCase):
     def setUp(self):
         self.addUserRepository = MagicMock(spec=AddUserRepository)
-        self.sut = DbAddUser(self.addUserRepository)
+        self.loadUserByEmailRepository = MagicMock(
+            spec=LoadUserByEmailRepository
+        )
+        self.sut = DbAddUser(
+            self.addUserRepository, self.loadUserByEmailRepository
+        )
 
     async def test_add_calls_addUserRepository_with_correct_params(self):
         user_data = {
@@ -29,3 +37,18 @@ class TestDbAddUser(unittest.IsolatedAsyncioTestCase):
         self.addUserRepository.add.return_value = user_data
         user = await self.sut.add(user_data)
         self.assertEqual(user, user_data)
+
+    async def test_should_call_loadUserByEmailRepository_with_correct_email(
+        self,
+    ):
+        user_data = {
+            "id": "any_id",
+            "fullname": "any_name",
+            "email": "any_email",
+            "password": "any_password",
+            "created_at": "any_created_at",
+        }
+        await self.sut.add(user_data)
+        self.loadUserByEmailRepository.load.assert_called_with(
+            user_data["email"]
+        )
