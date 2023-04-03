2a2be55572c1d9aa80ec7c0ed332b38a4bb95039
cassiusbessa <cassiusbessa@gmail.com>
2023-04-03 05:14:30 -0300

    test: :white_check_mark: ensure dbAuthLogin returns a Login response model if succesfully

--- src/tests/useCases/test_authLogin.py
+++ src/tests/useCases/test_authLogin.py
@@ -3,6 +3,7 @@ from unittest.mock import MagicMock
 from data.dbAuthLogin import DbAddLogin
 from data.protocols.cryptographyProtocols import TokenGenerator
 from data.protocols.userDbProtocols import LoadUserByEmailRepository
+from domain.entities.user import User
 
 
 class TestAuthLogin(unittest.IsolatedAsyncioTestCase):
@@ -35,4 +36,30 @@ class TestAuthLogin(unittest.IsolatedAsyncioTestCase):
         await self.sut.auth(credentials)
         self.tokenGenerator.generateToken.assert_called_with(
             await self.loadUserByEmailRepository.load(credentials["email"])
+        )
+
+    async def test_should_return_login_response_model(self):
+        credentials = {
+            "email": "any_email@email.com",
+            "password": "any_password",
+        }
+        self.loadUserByEmailRepository.load.return_value = User(
+            id="valid_id",
+            fullname="valid_name",
+            email="valid_email@email.com",
+            password="valid_password",
+            createdAt="valid_created_at",
+        )
+        self.tokenGenerator.generateToken.return_value = "valid_token"
+        login_response_model = await self.sut.auth(credentials)
+        self.assertEqual(
+            login_response_model,
+            {
+                "token": "valid_token",
+                "id": "valid_id",
+                "fullname": "valid_name",
+                "email": "valid_email@email.com",
+                "createdAt": "valid_created_at",
+                "password": "valid_password",
+            },
         )
