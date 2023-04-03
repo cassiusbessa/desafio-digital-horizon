8f6da010e9e00e278f43f73563c2a730306f33fd
cassiusbessa <cassiusbessa@gmail.com>
2023-04-03 19:27:44 -0300

    feat: :sparkles: ensure dbAddUser usecase calls loadUSerByEmailRepository with corrects params

--- src/data/dbAddUser.py
+++ src/data/dbAddUser.py
@@ -1,12 +1,21 @@
-from data.protocols.userDbProtocols import AddUserRepository
+from data.protocols.userDbProtocols import (
+    AddUserRepository,
+    LoadUserByEmailRepository,
+)
 from domain.entities.user import User
 from domain.usecases.addUser import AddUser, AddUserModel
 
 
 class DbAddUser(AddUser):
-    def __init__(self, addUserRepository: AddUserRepository):
+    def __init__(
+        self,
+        addUserRepository: AddUserRepository,
+        loadUserByEmailRepository: LoadUserByEmailRepository,
+    ):
         self.addUserRepository = addUserRepository
+        self.loadUserByEmailRepository = loadUserByEmailRepository
 
     async def add(self, userData: AddUserModel) -> User:
+        await self.loadUserByEmailRepository.load(userData["email"])
         user = await self.addUserRepository.add(userData)
         return user
