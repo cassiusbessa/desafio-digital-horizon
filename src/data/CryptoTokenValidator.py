2ae6600a518094913776809b20eab3ae784b1741
cassiusbessa <cassiusbessa@gmail.com>
2023-04-04 03:36:30 -0300

    feat: :sparkles: ensure that CryptoTokenValidator calls the decrypter with the specified parameters and returns the result obtained from it.

---
+++ src/data/CryptoTokenValidator.py
@@ -0,0 +1,10 @@
+from data.protocols.cryptographyProtocols import TokenDecrypter
+from domain.usecases.tokenValidator import TokenValidator
+
+
+class CryptoTokenValidator(TokenValidator):
+    def __init__(self, decrypter: TokenDecrypter):
+        self.decrypter = decrypter
+
+    async def validate(self, token: str) -> bool:
+        return await self.decrypter.decryptToken(token)
