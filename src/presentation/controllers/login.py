3b1ca91d619f9f8e07d19148840f14ad2e501d51
cassiusbessa <cassiusbessa@gmail.com>
2023-04-04 00:03:13 -0300

    feat: :sparkles: ensure Login returns a 403 code and a forbidden message if authentication returns none

--- src/presentation/controllers/login.py
+++ src/presentation/controllers/login.py
@@ -1,4 +1,5 @@
 from domain.usecases.authentication import Authentication
+from presentation.errors.invalidCredentialsError import InvalidCredentialsError
 from presentation.errors.missingParamError import MissingParamError
 from presentation.controllers.protocols.controller import (
     Controller,
@@ -8,6 +9,7 @@ from presentation.controllers.protocols.controller import (
 from presentation.controllers.protocols.responses import (
     badRequest,
     created,
+    forbidden,
     serverError,
 )
 
@@ -23,6 +25,8 @@ class Login(Controller):
                 if field not in httpRequest.body:
                     return badRequest(MissingParamError(field))
             payload = await self.authentication.auth(httpRequest.body)
+            if payload is None:
+                return forbidden(InvalidCredentialsError())
             return created(payload)
         except Exception as e:
             print(e)
