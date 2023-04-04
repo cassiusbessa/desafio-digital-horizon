3cedf8f43f7fea7efae425969e2d60da6fc6cdb4
cassiusbessa <cassiusbessa@gmail.com>
2023-04-04 03:55:08 -0300

    feat: :sparkles: ensure JwtAdapter returns true on valid token or false on invalid token

--- src/infra/cryptography/jwtAdapter.py
+++ src/infra/cryptography/jwtAdapter.py
@@ -1,11 +1,11 @@
 import datetime
 import jwt
-
 from data.protocols.cryptographyProtocols import TokenGenerator
 from domain.entities.user import User
+from domain.usecases.tokenValidator import TokenValidator
 
 
-class JwtAdapter(TokenGenerator):
+class JwtAdapter(TokenGenerator, TokenValidator):
     def __init__(self, secretKey: str):
         self.secretKey = secretKey
 
@@ -14,3 +14,10 @@ class JwtAdapter(TokenGenerator):
             payload.createdAt, "%Y-%m-%d %H:%M:%S"
         )
         return jwt.encode(payload.toDict(), self.secretKey, algorithm="HS256")
+
+    async def validateToken(self, token: str) -> bool:
+        try:
+            jwt.decode(token, self.secretKey, algorithms=["HS256"])
+            return True
+        except Exception:
+            return False
