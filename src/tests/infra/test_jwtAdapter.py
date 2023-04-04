23a7b71ef561a6f617ccc9f1ebdeaed442103c1b
cassiusbessa <cassiusbessa@gmail.com>
2023-04-04 03:55:21 -0300

    test: :white_check_mark: ensure JwtAdapter returns true on valid token or false on invalid token

--- src/tests/infra/test_jwtAdapter.py
+++ src/tests/infra/test_jwtAdapter.py
@@ -24,3 +24,17 @@ class TestJwtAdapter(unittest.IsolatedAsyncioTestCase):
         mockEncode.return_value = "token"
         token = await jwtAdapter.generateToken(self.user)
         self.assertEqual(token, "token")
+
+    @mock.patch.object(jwt, "decode")
+    async def test_should_return_true_on_decode(self, mockDecode):
+        jwtAdapter = JwtAdapter(secretKey=self.secretKey)
+        mockDecode.return_value = self.user.toDict()
+        isValid = await jwtAdapter.validateToken("token")
+        self.assertTrue(isValid)
+
+    @mock.patch.object(jwt, "decode")
+    async def test_should_return_false_on_decode(self, mockDecode):
+        jwtAdapter = JwtAdapter(secretKey=self.secretKey)
+        mockDecode.side_effect = Exception()
+        isValid = await jwtAdapter.validateToken("token")
+        self.assertFalse(isValid)
