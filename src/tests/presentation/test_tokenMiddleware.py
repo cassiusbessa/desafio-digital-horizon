28f6d55dcd814de1fda536cc14c5308f070e24c8
cassiusbessa <cassiusbessa@gmail.com>
2023-04-04 03:02:13 -0300

    test: :white_check_mark: ensure TokenMiddleware returns a status 401 and unauthorized message if token is invalid

--- src/tests/presentation/test_tokenMiddleware.py
+++ src/tests/presentation/test_tokenMiddleware.py
@@ -1,6 +1,7 @@
 import unittest
 from unittest.mock import MagicMock
 from domain.usecases.tokenValidator import TokenValidator
+from presentation.controllers.protocols.controller import Controller
 from presentation.controllers.protocols.http import HttpRequest
 
 from presentation.middleware.tokenMiddleware import TokenMiddleware
@@ -9,10 +10,18 @@ from presentation.middleware.tokenMiddleware import TokenMiddleware
 class TestTokenMiddleware(unittest.IsolatedAsyncioTestCase):
     def setUp(self):
         self.tokenValidator = MagicMock(spec=TokenValidator)
-        self.sut = TokenMiddleware(self.tokenValidator)
+        self.Controller = MagicMock(spec=Controller)
+        self.sut = TokenMiddleware(self.Controller, self.tokenValidator)
 
     async def test_handle_should_return_401_if_no_token_provided(self):
         request = HttpRequest(headers={"authorization": None})
+        response = await self.sut.handle(request)
+        self.assertEqual(response.statusCode, 401)
+        self.assertEqual(response.body, {"error": "Invalid credentials"})
+
+    async def test_handle_should_return_401_if_token_is_invalid(self):
+        request = HttpRequest(headers={"authorization": None})
+        self.tokenValidator.validate.return_value = False
         response = await self.sut.handle(request)
         self.assertEqual(response.statusCode, 401)
         self.assertEqual(response.body, {"error": "Invalid credentials"})
